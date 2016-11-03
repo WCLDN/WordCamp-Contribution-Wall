eafcb9794821040ee7ddcab84538011be6768fe7
LoreleiAurora <iam@l5i.me>
2016-11-03 18:40:44 +0000

    Rename dist as public

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ if (isDeveloping) {
 	app.use(middleware);
 	app.use(webpackHotMiddleware(compiler));
 	app.get('*', function response(req, res) {
-		res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+		res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'public/index.html')));
 		res.end();
 	});
 } else {
-	app.use(express.static(__dirname + '/dist'));
+	app.use(express.static(__dirname + '/public'));
 	app.get('*', function response(req, res) {
-		res.sendFile(path.join(__dirname, 'dist/index.html'));
+		res.sendFile(path.join(__dirname, 'public/index.html'));
 	});
 }
 
